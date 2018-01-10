183a066d0ff2c32e10a6e23f9b1c0d43d5a3d5ce
Unknown <royvano@hotmail.com>
2018-01-10 17:15:13 +0100

    Opties en commentaar toegevoegd, kleine bugs weg gehaald

--- library.js
+++ library.js
@@ -1,65 +1,111 @@
-var library = function(){
+var cryptoTools = function(){
 
-var checkboxManager = function(){
-    var coins = ["bitcoin", "ripple", "gas", "ethereum", "cardano", "verge" , "litecoin", "stellar" ];
-    var coinChosen = [];
+/**
+     * Default setting for the module
+     * @type {{option: string(price or market_cap), barDirection: string(vertical or horizontal)}}
 
-    var arrayLength = coins.length ;
-      for (var i = 0; i < arrayLength; i++) {
-          //kijken of checkbox is aangevinkt
-          var coinChecked = document.getElementById(coins[i]).checked;
-        
-          if(coinChecked == true)
-          {
-            coinChosen.push(coins[i]);
-          }
-      }    
+     */
+    var defaultSettings = {
+        option: 'price',
+        barDirection: 'horizontal'
+    };
 
-      var arrayLengthChecked = coinChosen.length;
-      if (arrayLengthChecked > 0) {
-        var lengthIndexed = arrayLengthChecked;
-        for (var i = 0; i < lengthIndexed; i++){
-          apiCall("https://api.coinmarketcap.com/v1/ticker/" + coinChosen[i] + "/");
-         }
+//Function checkboxManager checks which boxes are ticked and collects ticked items in coinChosen Array
+    var checkboxManager = function(){
+      var coins = ["bitcoin", "ripple", "gas", "ethereum", "cardano", "verge" , "litecoin", "stellar" ];
+      var coinChosen = [];
+      var arrayLengthChecked = 0;
+
+      var arrayLength = coins.length ;
+        for (var i = 0; i < arrayLength; i++) {
+            //Check if checkbox is ticked
+            var coinChecked = document.getElementById(coins[i]).checked;
+            if(coinChecked == true)
+            {
+              coinChosen.push(coins[i]);
+            }
+        }    
+
+        arrayLengthChecked = coinChosen.length;
+
+        if (arrayLengthChecked > 0 && arrayLengthChecked == 4) {
+          for (var i = 0; i < arrayLengthChecked; i++){
+            apiCall("https://api.coinmarketcap.com/v1/ticker/" + coinChosen[i] + "/");
+           }
+         } else {
+              document.getElementById("alerto").innerHTML = "Kies Precies 4 Coins";
+            }
       }
-    }
 
+  //Variable for saving json data
   var dataJSON;
+  //Empty array for multiple json objects
+  var jsonArray = [];
 
+  //Converting the JSON data from request to an array which is used for setting up google charts. 
   var processData = function(data){
     obj = JSON.parse(data);
     var obj0 = obj[0];
+    jsonArray.push(obj0);
 
     dataJSON = data;
     google.charts.load('current', {'packages':['bar']});
         google.charts.setOnLoadCallback(drawChart);
   }
 
-  var drawChart = function() {
-      obj = JSON.parse(dataJSON);
-      var obj0 = obj[0];
-      console.log(obj0);
-        
-      var data = google.visualization.arrayToDataTable([
-            ['Names', 'Price in $', 'Change in %', 'Marketcap'],
-            [obj0.name, obj0.price_usd, obj0.percent_change_24h, obj0.market_cap_usd],
-            [obj0.name, obj0.price_usd, obj0.percent_change_24h, obj0.market_cap_usd],
-            [obj0.name, obj0.price_usd, obj0.percent_change_24h, obj0.market_cap_usd],
-            [obj0.name, obj0.price_usd, obj0.percent_change_24h, obj0.market_cap_usd]
-          ]);
+  //Function drawChart, converts & sorts data and draws it in the chart
+  var drawChart = function() { 
 
+      var data;  
+      document.getElementById("alerto").innerHTML = "";
+      if (defaultSettings.option ==  "price" && jsonArray[3].name != null){
+          data = google.visualization.arrayToDataTable([
+            ['Coins', "Price in $"],
+            [jsonArray[0].name, parseFloat(jsonArray[0].price_usd)],
+            [jsonArray[1].name, parseFloat(jsonArray[1].price_usd)],
+            [jsonArray[2].name, parseFloat(jsonArray[2].price_usd)],
+            [jsonArray[3].name, parseFloat(jsonArray[3].price_usd)]
+          ]);
           var options = {
             chart: {
-              title: 'Crpyto Ticker',
+              title: 'Crypto Ticker',
               subtitle: "Choose and watch different Crypto's live!",
-            }
+            },
+            bars: defaultSettings.barDirection,
+          vAxis: {format: 'decimal'},
+          height: 400,
+          colors: ['#1b9e77', '#d95f02', '#7570b3']
           };
 
           var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+          data.sort({column: 0, desc: true});
+          chart.draw(data, google.charts.Bar.convertOptions(options));
+      } else if (defaultSettings.option == "market_cap" && jsonArray.length >= 4){
+      data = google.visualization.arrayToDataTable([
+            ['Coins', "Market Cap in $"],
+            [jsonArray[0].name, parseFloat(jsonArray[0].market_cap_usd)],
+            [jsonArray[1].name, parseFloat(jsonArray[1].market_cap_usd)],
+            [jsonArray[2].name, parseFloat(jsonArray[2].market_cap_usd)],
+            [jsonArray[3].name, parseFloat(jsonArray[3].market_cap_usd)]
+          ]);
+      var options = {
+            chart: {
+              title: 'Crypto Ticker',
+              subtitle: "Choose and watch different Crypto's live!",
+            },
+            bars: defaultSettings.barDirection,
+          vAxis: {format: 'decimal'},
+          height: 400,
+          colors: ['#1b9e77', '#d95f02', '#7570b3']
+          };
 
-          chart.draw(data);
-        }
+          var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+          data.sort({column: 0, desc: true});
+          chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+   }
 
+    //Function apiCall handles sending and receiving the API requests
     var apiCall = function(url){
       var request = new XMLHttpRequest();
       request.onreadystatechange = function() {
@@ -81,8 +127,7 @@ var checkboxManager = function(){
 
 /**
      * Initializes the module
-     * @param {string} [selector] Css selector that targets the element that needs to bounce
-     * @param {object} [settings] Object that contain overrides for the default settings
+     * @param {string} [url] string containing API url
      */
     var init = function(url) {
         checkboxManager();
